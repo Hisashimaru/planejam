31eb8151d4fb991420c4a85df3c89fc66c6a2136
Hisashimaru <cky.cola@gmail.com>
2021-01-15 13:40:22 +0900

    hide shop button sprite when use the keyboard

---
+++ Assets/Scripts/UI/ButtonSprite.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSprite : MonoBehaviour
+{
+	public Sprite keyboardSprite;
+	public Sprite gamepadSprite;
+	Image image;
+
+	void Awake()
+	{
+		image = GetComponent<Image>();
+	}
+
+	void Update()
+	{
+		if(image == null)
+			return;
+
+		if(NS.Gamepad.useGamepad)
+		{
+			if(gamepadSprite != null)
+			{
+				image.sprite = gamepadSprite;
+				image.enabled = true;
+			}
+			else
+			{
+				image.enabled = false;
+			}
+			Debug.Log("PAD");
+		}
+		else
+		{
+			if(keyboardSprite != null)
+			{
+				image.sprite = keyboardSprite;
+				image.enabled = true;
+			}
+			else
+			{
+				image.enabled = false;
+			}
+		}
+	}
+}
